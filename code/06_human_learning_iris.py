b054d8e62f0f8348d9de1a91b6cf5796f6d9e87b
Bryan R Gibson <bryan.r.gibson@gmail.com>
2015-11-05 20:37:02 -0500

    adding iris task

---
+++ code/06_human_learning_iris.py
@@ -0,0 +1,14 @@
+'''
+EXERCISE: "Human Learning" with iris data
+
+Can you predict the species of an iris using petal and sepal measurements?
+
+TASKS:
+1. Read the iris data into a pandas DataFrame, including column names.
+2. Gather some basic information about the data.
+3. Use groupby, sorting, and plotting to look for differences between species.
+4. Write down a set of rules that could be used to predict species based on measurements.
+
+BONUS: Define a function that accepts a row of data and returns a predicted species.
+Then, use that function to make predictions for all existing rows of data.
+'''
